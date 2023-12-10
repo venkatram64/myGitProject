6797985a341d82393043799c94763469d2583d7f
venkatram64 <venkat.veerareddy@hotmail.com>
2023-12-10 09:58:51 +0530

    VST-1234 added functionality

---
+++ src/main/java/com/venkat/service/Calculator.java
@@ -0,0 +1,16 @@
+package com.venkat.service;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+
+    public int division(int a, int b){
+        return a/b;
+    }
+}
